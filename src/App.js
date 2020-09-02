44b85b59a553c85d9742ccf28becabdae6d35599
Vamsi Charan Adari <adarivamsi@gmail.com>
2020-09-01 21:44:15 -0400

    Added header.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './components/shared/header/header';
+import './App.css';
 
 function App() {
   return (
     <div className="container">
-        
+        <Header />
     </div>
   );
 }
